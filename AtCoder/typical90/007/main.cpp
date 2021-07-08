705c0721f02f6e2cc2ac30f007c127226fd20573
fumito <a.t.h.t.fumi@gmail.com>
2021-07-08 22:04:16 +0900

    typical90

--- AtCoder/typical90/007/main.cpp
+++ AtCoder/typical90/007/main.cpp
@@ -4,23 +4,36 @@ typedef long long ll;
 typedef vector<ll> vll;
 typedef vector<vll> vvll;
 
-
-
-void solve(long long N, std::vector<long long> A, long long Q, std::vector<long long> B){
-
+void solve(long long N, std::vector<long long> A, long long Q,
+           std::vector<long long> B) {
+    sort(A.begin(), A.end());
+    for (ll i = 0; i < Q; i++) {
+        auto ite = lower_bound(A.begin(), A.end(), B[i]);
+        ll ans = 0;
+        if (ite == A.end())
+            ans = B[i] - A[N - 1];
+        else if (ite == A.begin())
+            ans = A[0] - B[i];
+        else {
+            ll x = *ite;
+            ll y = *(--ite);
+            ans = min(x - B[i], B[i] - y);
+        }
+        printf("%lld\n", ans);
+    }
 }
 
-int main(){
+int main() {
     long long N;
     std::scanf("%lld", &N);
     std::vector<long long> A(N);
-    for(int i = 0 ; i < N ; i++){
+    for (int i = 0; i < N; i++) {
         std::scanf("%lld", &A[i]);
     }
     long long Q;
     std::scanf("%lld", &Q);
     std::vector<long long> B(Q);
-    for(int i = 0 ; i < Q ; i++){
+    for (int i = 0; i < Q; i++) {
         std::scanf("%lld", &B[i]);
     }
     solve(N, std::move(A), Q, std::move(B));
--- AtCoder/typical90/010/main.cpp
+++ AtCoder/typical90/010/main.cpp
@@ -4,18 +4,33 @@ typedef long long ll;
 typedef vector<ll> vll;
 typedef vector<vll> vvll;
 
+void solve(long long N, std::vector<long long> C, std::vector<long long> P,
+           long long Q, std::vector<long long> L, std::vector<long long> R) {
+    vll one(P), two(P);
+    for (ll i = 0; i < N; i++) {
+        if (C[i] == 1)
+            two[i] = 0;
+        else
+            one[i] = 0;
+    }
+    vll sumone(N + 1, 0), sumtwo(N + 1, 0);
+    for (ll i = 0; i < N; i++) {
+        sumone[i + 1] = sumone[i] + one[i];
+        sumtwo[i + 1] = sumtwo[i] + two[i];
+    }
 
-
-void solve(long long N, std::vector<long long> C, std::vector<long long> P, long long Q, std::vector<long long> L, std::vector<long long> R){
-
+    for (ll i = 0; i < Q; i++) {
+        printf("%Ld %Ld\n", sumone[R[i]] - sumone[L[i] - 1],
+               sumtwo[R[i]] - sumtwo[L[i] - 1]);
+    }
 }
 
-int main(){
+int main() {
     long long N;
     std::scanf("%lld", &N);
     std::vector<long long> C(N);
     std::vector<long long> P(N);
-    for(int i = 0 ; i < N ; i++){
+    for (int i = 0; i < N; i++) {
         std::scanf("%lld", &C[i]);
         std::scanf("%lld", &P[i]);
     }
@@ -23,7 +38,7 @@ int main(){
     std::scanf("%lld", &Q);
     std::vector<long long> L(Q);
     std::vector<long long> R(Q);
-    for(int i = 0 ; i < Q ; i++){
+    for (int i = 0; i < Q; i++) {
         std::scanf("%lld", &L[i]);
         std::scanf("%lld", &R[i]);
     }
