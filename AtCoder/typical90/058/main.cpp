ece10240cde64b7cd3050671baec81da06dad5b3
fumito <a.t.h.t.fumi@gmail.com>
2021-08-26 22:26:30 +0900

    typical90

--- AtCoder/typical90/058/main.cpp
+++ AtCoder/typical90/058/main.cpp
@@ -4,14 +4,44 @@ typedef long long ll;
 typedef vector<ll> vll;
 typedef vector<vll> vvll;
 
+const long long MOD = 100000;
 
-const long long MOD = 10;
-
-void solve(long long N, long long K){
-
+ll update(ll x) {
+    ll y = 0, t = x;
+    do {
+        y += t % 10;
+        t /= 10;
+    } while (t > 0);
+    return (x + y) % MOD;
 }
 
-int main(){
+void solve(long long N, long long K) {
+    vll res;
+    set<ll> s;
+    for (ll i = 0; i < 100010; i++) {
+        if (s.find(N) != s.end()) break;
+        res.push_back(N);
+        s.insert(N);
+        N = update(N);
+    }
+    ll idx = 0;
+    for (ll i = 0; i < res.size(); i++) {
+        if (res[i] == N) idx = i;
+    }
+    ll ans;
+    if (K < idx) {
+        ans = res[K];
+    } else if (K < res.size()) {
+        ans = res[K];
+    } else {
+        ll cycle = res.size() - idx;
+        K = idx + ((K - idx) % cycle);
+        ans = res[K];
+    }
+    cout << ans << endl;
+}
+
+int main() {
     long long N;
     std::scanf("%lld", &N);
     long long K;
